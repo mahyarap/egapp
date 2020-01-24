3a823e6b269b68cd1cb833e6b91cdd920b14ca73
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2020-01-25 02:52:30 +0330

    Add tests for fsm

--- test/egapp/parser/fsm_test.exs
+++ test/egapp/parser/fsm_test.exs
@@ -1,45 +1,19 @@
 defmodule Egapp.Parser.FSMTest do
   use ExUnit.Case, async: true
 
+  alias Egapp.Parser.XML.FSM
+  alias Egapp.Parser.XML.EventMan
+
   setup do
-    {:ok, event_man} = GenServer.start_link(Egapp.Parser.EventManager, to: self(), mod: Kernel)
+    event_man = start_supervised!({EventMan, to: self(), mod: Kernel})
     {:ok, event_man: event_man}
   end
 
-  test "can handle xml version", context do
-    {:ok, fsm} =
-      :gen_fsm.start_link(
-        Egapp.Parser.FSM,
-        [event_man: context[:event_man], init_state: :begin],
-        []
-      )
-
-    xml_declaration = """
-    <?xml version="1.0"?>\
-    """
-
-    :fxml_stream.new(fsm)
-    |> :fxml_stream.parse(xml_declaration)
-
-    result =
-      receive do
-        msg -> msg
-      after
-        10 -> :timeout
-      end
-
-    assert ^result = :timeout
-  end
-
-  test "can transition to initial state", context do
-    {:ok, fsm} =
-      :gen_fsm.start_link(
-        Egapp.Parser.FSM,
-        [event_man: context[:event_man], init_state: :begin],
-        []
-      )
+  test "can transition to initial state", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
 
     stream = """
+    <?xml version="1.0"?>
     <stream:stream
     to="example.com"
     xmlns="jabber:client"
@@ -47,75 +21,124 @@ defmodule Egapp.Parser.FSMTest do
     version="1.0">\
     """
 
+    assert {:begin, _} = :sys.get_state(fsm)
+
     :fxml_stream.new(fsm)
     |> :fxml_stream.parse(stream)
 
     assert {:xml_stream_start, _} = :sys.get_state(fsm)
-
-    result =
-      receive do
-        msg -> msg
-      after
-        10 -> raise "foo"
-      end
-
-    assert result =~ ~s(<?xml version="1.0"?>)
-    assert result =~ ~s(<stream:stream)
-    assert result =~ ~s(version="1.0")
-    assert result =~ ~s(xml:lang="en")
+    assert_received resp
+    assert not Enum.empty?(resp)
   end
 
-  # test "can transition to second state", context do
-  #   {:ok, fsm} = :gen_fsm.start_link(
-  #     Egapp.Parser.FSM,
-  #     [event_man: context[:event_man], init_state: :begin],
-  #     []
-  #   )
-  #   foo = """
-  #   <stream:stream
-  #   to="example.com"
-  #   xmlns="jabber:client"
-  #   xmlns:stream="http://etherx.jabber.org/streams"
-  #   version="1.0">\
-  #   """
-  #   bar = """
-  #   <iq>foo</iq>\
-  #   """
-  #   resp = """
-  #   <iq>an</iq>
-  #   """
-  #   stream = :fxml_stream.new(fsm)
-  #   stream = :fxml_stream.parse(stream, foo)
-  #   stream = :fxml_stream.parse(stream, bar)
-  #   assert_receive ^resp, 10
-  # end
+  test "stream with syntax error", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
 
-  # test "can transition to third state", context do
-  #   {:ok, fsm} = :gen_fsm.start_link(
-  #     Egapp.Parser.FSM,
-  #     [event_man: context[:event_man], init_state: :begin],
-  #     []
-  #   )
-  #   foo = """
-  #   <stream:stream
-  #   to="example.com"
-  #   xmlns="jabber:client"
-  #   xmlns:stream="http://etherx.jabber.org/streams"
-  #   version="1.0">\
-  #   """
-  #   bar = """
-  #   <iq>foo</iq>\
-  #   """
-  #   baz = """
-  #   <message>hi</message>\
-  #   """
-  #   resp = """
-  #   <message>hoy</message>
-  #   """
-  #   stream = :fxml_stream.new(fsm)
-  #   stream = :fxml_stream.parse(stream, foo)
-  #   stream = :fxml_stream.parse(stream, bar)
-  #   stream = :fxml_stream.parse(stream, baz)
-  #   assert_receive ^resp, 10
-  # end
+    stream = "syntax error"
+
+    :fxml_stream.new(fsm)
+    |> :fxml_stream.parse(stream)
+
+    catch_exit :sys.get_state(fsm)
+    assert_received resp
+
+    resp = IO.chardata_to_string(resp)
+    assert resp =~ "<bad-format"
+  end
+
+  test "not well formed stream", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
+
+    stream = """
+    <?>
+    """
+
+    :fxml_stream.new(fsm)
+    |> :fxml_stream.parse(stream)
+
+    catch_exit :sys.get_state(fsm)
+    assert_received resp
+
+    resp = IO.chardata_to_string(resp)
+    assert resp =~ "<not-well-formed"
+  end
+
+  test "stream with unbound prefix", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
+
+    stream = """
+    <stream:stream>
+    """
+
+    :fxml_stream.new(fsm)
+    |> :fxml_stream.parse(stream)
+
+    catch_exit :sys.get_state(fsm)
+    assert_received resp
+
+    resp = IO.chardata_to_string(resp)
+    assert resp =~ "<not-well-formed"
+  end
+
+  test "stream with duplicate attributes", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
+
+    stream = """
+    <stream:stream foo="bar" foo="bar">
+    """
+
+    :fxml_stream.new(fsm)
+    |> :fxml_stream.parse(stream)
+
+    catch_exit :sys.get_state(fsm)
+    assert_received resp
+
+    resp = IO.chardata_to_string(resp)
+    assert resp =~ "<not-well-formed"
+  end
+
+  test "no stream tag", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
+
+    stream = "<foo>"
+
+    :fxml_stream.new(fsm)
+    |> :fxml_stream.parse(stream)
+
+    catch_exit :sys.get_state(fsm)
+    assert_received resp
+
+    resp = IO.chardata_to_string(resp)
+    assert resp =~ "<not-well-formed"
+  end
+
+  @tag :skip
+  test "can transition to second state", %{event_man: event_man} do
+    fsm = start_supervised!({FSM, event_man: event_man, init_state: :begin})
+
+    foo = """
+    <stream:stream
+    to="example.com"
+    xmlns="jabber:client"
+    xmlns:stream="http://etherx.jabber.org/streams"
+    version="1.0">\
+    """
+
+    bar = """
+    <iq type='set' id='purple35aab1c4'><session xmlns='urn:ietf:params:xml:ns:xmpp-session'/></iq>\
+    """
+
+    assert {:begin, _} = :sys.get_state(fsm)
+    stream =
+    :fxml_stream.new(fsm)
+    |> :fxml_stream.parse(foo)
+
+    assert {:xml_stream_start, _} = :sys.get_state(fsm)
+    :fxml_stream.parse(stream, bar)
+
+    assert {:xml_stream_element, _} = :sys.get_state(fsm)
+
+    assert_receive resp
+    assert not Enum.empty?(resp)
+  end
 end
