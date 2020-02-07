bbe04d28ea23cc29e8368a4ef80c92dccfb352fe
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2020-02-08 03:26:35 +0330

    Add :egapp config to the test config

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,10 @@ config :egapp, Egapp.Repo,
   pool: Ecto.Adapters.SQL.Sandbox
 
 config :egapp,
-  ecto_repos: [Egapp.Repo]
+  ecto_repos: [Egapp.Repo],
+  address: "0.0.0.0",
+  port: 0,
+  domain_name: "egapp.im"
 
 config :logger,
   backends: [:console],
