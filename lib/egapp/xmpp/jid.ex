3f50a0833fb0e3884fcaf30c12a05637e471a3c1
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2020-02-08 18:21:09 +0330

    Add partial parsing to the Jid module

--- lib/egapp/xmpp/jid.ex
+++ lib/egapp/xmpp/jid.ex
@@ -2,6 +2,7 @@ defmodule Egapp.XMPP.Jid do
   defstruct [:localpart, :domainpart, :resourcepart]
 
   @jid_re ~r|^(?<localpart>[^@]+)@(?<domainpart>[^/]+)(/(?<resourcepart>.*))?$|
+  @partial_jid_re ~r|^((?<localpart>[^@]+)@)?(?<domainpart>[^/]+)(/(?<resourcepart>.*))?$|
 
   def bare_jid(jid) do
     jid.localpart <> "@" <> jid.domainpart
@@ -12,9 +13,17 @@ defmodule Egapp.XMPP.Jid do
   end
 
   def parse(str) do
-    result = Regex.named_captures(@jid_re, str)
+    do_parse(str, @jid_re)
+  end
 
-    %Egapp.XMPP.Jid{
+  def partial_parse(str) do
+    do_parse(str, @partial_jid_re)
+  end
+
+  defp do_parse(str, re) do
+    result = Regex.named_captures(re, str)
+
+    %__MODULE__{
       localpart: result["localpart"],
       domainpart: result["domainpart"],
       resourcepart: result["resourcepart"]
