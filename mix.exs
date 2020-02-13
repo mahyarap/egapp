79580d9c2eb802ff7a90fff96a992ffc8306d0cc
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2020-02-13 14:00:55 +0330

    Add credo for lint suggestions

--- mix.exs
+++ mix.exs
@@ -26,7 +26,8 @@ defmodule Egapp.MixProject do
       {:fast_xml, "~> 1.1"},
       # {:fast_xml, git: "https://github.com/processone/fast_xml.git", tag: "1.1.37"},
       {:ecto_sql, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"}
+      {:postgrex, ">= 0.0.0"},
+      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
