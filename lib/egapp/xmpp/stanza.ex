mahyarap/egapp

commit da1804bd840fc0b9e3160edc6a65a1c88cbd3a39

Ensure elements return status code to upper layer
Ensure the elements return status to wrapper stanzas so that every layer
generates its own errors.

lib/egapp/xmpp/stanza.ex
@@ -42,6 +42,12 @@ defmodule Egapp.XMPP.Stanza do
   end
 
   def build_iq_attrs(attrs, type, state) do
+    type =
+      case type do
+        :ok -> 'result'
+        :error -> 'error'
+      end
+
     %{
       lang: Map.get(state.client, "xml:lang", "en"),
       id: Map.get(attrs, "id"),

test/egapp/xmpp/fsm_test.exs
@@ -161,7 +161,7 @@ defmodule Egapp.XMPP.FSMTest do
     data = [{:xmlel, "bind", [{"xmlns", Const.xmlns_bind()}], []}]
 
     assert :continue = :gen_statem.call(fsm, {"iq", attrs, data})
-    assert_received resp
+    assert_received [{_, resp}]
     resp = IO.chardata_to_string(resp)
     assert resp =~ "<bind"
     assert resp =~ "<jid"

test/egapp/xmpp/stanza_test.exs
@@ -28,7 +28,7 @@ defmodule Egapp.XMPP.StanzaTest do
     jid = %Jid{localpart: "foo", domainpart: "bar", resourcepart: "123"}
     state = put_in(state, [:client, :jid], jid)
 
-    assert {:ok, resp} = Stanza.iq(attrs, child, state)
+    assert {:ok, [{_, resp}]} = Stanza.iq(attrs, child, state)
     resp = IO.chardata_to_string(resp)
 
     assert resp =~ ~s(<iq)
@@ -97,7 +97,7 @@ defmodule Egapp.XMPP.StanzaTest do
     attrs = %{"type" => "get", "id" => state.id, "to" => "conference.egapp.im"}
     child = {"query", %{"xmlns" => Const.xmlns_disco_info()}, []}
     state = Map.put(state, :cats, [Egapp.XMPP.Server, Egapp.XMPP.Conference])
-    assert {:ok, [{_, resp} ]}= Stanza.iq(attrs, child, state)
+    assert {:ok, [{_, resp} ]} = Stanza.iq(attrs, child, state)
     resp = IO.chardata_to_string(resp)
 
     assert resp =~ ~s(<iq)
@@ -154,7 +154,7 @@ defmodule Egapp.XMPP.StanzaTest do
     attrs = %{"type" => "get", "id" => state.id}
     child = {"time", %{"xmlns" => Const.xmlns_time()}, []}
     state = put_in(state, [:client, :id], 1)
-    assert {:ok, resp} = Stanza.iq(attrs, child, state)
+    assert {:ok, [{_, resp}]} = Stanza.iq(attrs, child, state)
     resp = IO.chardata_to_string(resp)
 
     assert resp =~ ~s(<iq)

lib/egapp/xmpp/conference/stanza.ex
@@ -4,11 +4,13 @@ defmodule Egapp.XMPP.Conference.Stanza do
   alias Egapp.XMPP.Conference.Element
 
   def iq(%{"type" => "get"} = attrs, {"query", child_attrs, child_data}, state) do
+    {status, result} = Element.query(child_attrs, child_data, state)
+
     resp =
-      Element.query(child_attrs, child_data, state)
+      result
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'result', state), content)
+          iq_template(build_iq_attrs(attrs, status, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}

lib/egapp/xmpp/server/stanza.ex
@@ -17,11 +17,13 @@ defmodule Egapp.XMPP.Server.Stanza do
   alias Egapp.XMPP.Server.Element
 
   def iq(%{"type" => "get"} = attrs, {"query", child_attrs, child_data}, state) do
+    {status, result} = Element.query(child_attrs, child_data, state)
+
     resp =
-      Element.query(child_attrs, child_data, state)
+      result
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'result', state), content)
+          iq_template(build_iq_attrs(attrs, status, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}
@@ -31,11 +33,13 @@ defmodule Egapp.XMPP.Server.Stanza do
   end
 
   def iq(%{"type" => "get"} = attrs, {"vCard", child_attrs, child_data}, state) do
+    {status, result} = Element.vcard(child_attrs, child_data, state)
+
     resp =
-      Element.vcard(child_attrs, child_data, state)
+      result
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'result', state), content)
+          iq_template(build_iq_attrs(attrs, status, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}
@@ -45,21 +49,29 @@ defmodule Egapp.XMPP.Server.Stanza do
   end
 
   def iq(%{"type" => "get"} = attrs, {"time", child_attrs, child_data}, state) do
-    content = Element.time(child_attrs, child_data, state)
+    {status, result} = Element.time(child_attrs, child_data, state)
 
     resp =
-      iq_template(build_iq_attrs(attrs, 'result', state), content)
-      |> :xmerl.export_simple_element(:xmerl_xml)
+      result
+      |> Enum.map(fn {conn, content} ->
+        resp =
+          iq_template(build_iq_attrs(attrs, status, state), content)
+          |> :xmerl.export_simple_element(:xmerl_xml)
+
+        {conn, resp}
+      end)
 
     {:ok, resp}
   end
 
   def iq(%{"type" => "get"} = attrs, {"ping", child_attrs, child_data}, state) do
+    {status, result} = Element.ping(child_attrs, child_data, state)
+
     resp =
-      Element.ping(child_attrs, child_data, state)
+      result
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'result', state), content)
+          iq_template(build_iq_attrs(attrs, status, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}
@@ -75,11 +87,13 @@ defmodule Egapp.XMPP.Server.Stanza do
           {tag_name, to_map(child_attrs), child_data}
       end
 
+    {status, result} = Element.query(child_attrs, child_data, state)
+
     resp =
-      Element.query(child_attrs, child_data, state)
+      result
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'result', state), content)
+          iq_template(build_iq_attrs(attrs, status, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}
@@ -89,21 +103,29 @@ defmodule Egapp.XMPP.Server.Stanza do
   end
 
   def iq(%{"type" => "set"} = attrs, {"bind", child_attrs, child_data}, state) do
-    content = Element.bind(child_attrs, child_data, state)
+    {status, result} = Element.bind(child_attrs, child_data, state)
 
     resp =
-      iq_template(build_iq_attrs(attrs, 'result', state), content)
-      |> :xmerl.export_simple_element(:xmerl_xml)
+      result
+      |> Enum.map(fn {conn, content} ->
+        resp =
+          iq_template(build_iq_attrs(attrs, status, state), content)
+          |> :xmerl.export_simple_element(:xmerl_xml)
+
+        {conn, resp}
+      end)
 
     {:ok, resp}
   end
 
   def iq(%{"type" => "set"} = attrs, {"session", child_attrs, child_data}, state) do
+    {status, result} = Element.session(child_attrs, child_data, state)
+
     resp =
-      Element.session(child_attrs, child_data, state)
+      result
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'result', state), content)
+          iq_template(build_iq_attrs(attrs, status, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}
@@ -117,7 +139,7 @@ defmodule Egapp.XMPP.Server.Stanza do
       [{state.to, Egapp.XMPP.Element.bad_request_error(:modify, "unknown element: #{element}")}]
       |> Enum.map(fn {conn, content} ->
         resp =
-          iq_template(build_iq_attrs(attrs, 'error', state), content)
+          iq_template(build_iq_attrs(attrs, :error, state), content)
           |> :xmerl.export_simple_element(:xmerl_xml)
 
         {conn, resp}

lib/egapp/xmpp/conference/element.ex
@@ -26,6 +26,6 @@ defmodule Egapp.XMPP.Conference.Element do
       |> hd()
 
     resp = Element.query_template([xmlns: Const.xmlns_disco_info()], content)
-    [{state.to, resp}]
+    {:ok, [{state.to, resp}]}
   end
 end

lib/egapp/xmpp/fsm.ex
@@ -141,7 +141,9 @@ defmodule Egapp.XMPP.FSM do
   def bind({:call, from}, {"iq", attrs, [{:xmlel, "bind", child_attrs, child_data}]}, state) do
     child_node = {"bind", to_map(child_attrs), child_data}
     {status, resp} = Stanza.iq(attrs, child_node, state)
-    apply(state.mod, :send, [state.to, resp])
+    Enum.each(resp, fn {conn, content} ->
+      apply(state.mod, :send, [conn, content])
+    end)
 
     case status do
       :ok -> {:next_state, :stanza, state, {:reply, from, :continue}}

lib/egapp/xmpp/server/element.ex
@@ -13,7 +13,7 @@ defmodule Egapp.XMPP.Server.Element do
       |> Enum.map(fn cat -> Element.item([jid: cat.address()], []) end)
 
     resp = query_template([xmlns: Const.xmlns_disco_items()], content)
-    [{state.to, resp}]
+    {:ok, [{state.to, resp}]}
   end
 
   def query(%{"xmlns" => Const.xmlns_disco_info()}, _data, state) do
@@ -24,7 +24,7 @@ defmodule Egapp.XMPP.Server.Element do
       |> Kernel.hd()
 
     resp = query_template([xmlns: Const.xmlns_disco_info()], content)
-    [{state.to, resp}]
+    {:ok, [{state.to, resp}]}
   end
 
   @doc """
@@ -54,7 +54,7 @@ defmodule Egapp.XMPP.Server.Element do
       end)
 
     resp = query_template([xmlns: Const.xmlns_roster()], items)
-    [{state.to, resp}]
+    {:ok, [{state.to, resp}]}
   end
 
   def query(
@@ -131,7 +131,7 @@ defmodule Egapp.XMPP.Server.Element do
     ]
 
     resp = query_template([xmlns: Const.xmlns_version()], content)
-    [{state.to, resp}]
+    {:ok, [{state.to, resp}]}
   end
 
   def query(%{"xmlns" => Const.xmlns_last()}, _data, _state) do
@@ -147,14 +147,16 @@ defmodule Egapp.XMPP.Server.Element do
   def bind(_attrs, _data, state) do
     full_jid = Jid.full_jid(state.client.jid) |> String.to_charlist()
 
-    {
+    resp = {
       :bind,
       [xmlns: Const.xmlns_bind()],
       [{:jid, [], [full_jid]}]
     }
+
+    {:ok, [{state.to, resp}]}
   end
 
-  def bind() do
+  def bind do
     {
       :bind,
       [xmlns: Const.xmlns_bind()],
@@ -162,7 +164,7 @@ defmodule Egapp.XMPP.Server.Element do
     }
   end
 
-  def session(_attrs, _data, state), do: [{state.to, []}]
+  def session(_attrs, _data, state), do: {:ok, [{state.to, []}]}
 
   def session do
     {
@@ -179,7 +181,7 @@ defmodule Egapp.XMPP.Server.Element do
       []
     }
 
-    [{state.to, resp}]
+    {:ok, [{state.to, resp}]}
   end
 
   def ping(_attrs, _data, state) do
@@ -192,11 +194,11 @@ defmodule Egapp.XMPP.Server.Element do
     [{state.to, resp}]
   end
 
-  def time(_attrs, _data, _state) do
+  def time(_attrs, _data, state) do
     {:ok, now} = DateTime.now("Etc/UTC")
     iso_time = DateTime.to_iso8601(now)
 
-    {
+    resp = {
       :time,
       [xmlns: Const.xmlns_time()],
       [
@@ -204,5 +206,6 @@ defmodule Egapp.XMPP.Server.Element do
         {:utc, [String.to_charlist(iso_time)]}
       ]
     }
+    {:ok, [{state.to, resp}]}
   end
 end

commit a8ffb91782e0ab5464b921e91e48ec9ea6c6c4cd

Fix regression introduced in da1804b

lib/egapp/xmpp/stanza.ex
@@ -28,7 +28,7 @@ defmodule Egapp.XMPP.Stanza do
   end
 
   def iq(attrs, _data, state) do
-    {:error, Stream.error(:invalid_xml, attrs, state)}
+    {:error, [{state.to, Stream.error(:invalid_xml, attrs, state)}]}
   end
 
   def iq_template(%{id: id, type: type, from: from}, content) do

test/egapp/xmpp/fsm_test.exs
@@ -161,7 +161,7 @@ defmodule Egapp.XMPP.FSMTest do
     data = [{:xmlel, "bind", [{"xmlns", Const.xmlns_bind()}], []}]
 
     assert :continue = :gen_statem.call(fsm, {"iq", attrs, data})
-    assert_received [{_, resp}]
+    assert_received resp
     resp = IO.chardata_to_string(resp)
     assert resp =~ "<bind"
     assert resp =~ "<jid"

test/egapp/xmpp/stanza_test.exs
@@ -229,7 +229,7 @@ defmodule Egapp.XMPP.StanzaTest do
     attrs = %{"id" => state.id}
     child = {"ping", %{"xmlns" => Const.xmlns_ping()}, []}
 
-    assert {:error, resp} = Stanza.iq(attrs, child, state)
+    assert {:error, [{_, resp}]} = Stanza.iq(attrs, child, state)
     resp = IO.chardata_to_string(resp)
 
     assert resp =~ ~s(<stream:error)
@@ -240,7 +240,7 @@ defmodule Egapp.XMPP.StanzaTest do
     attrs = %{"type" => "foo"}
     child = {"ping", %{"xmlns" => Const.xmlns_ping()}, []}
 
-    assert {:error, resp} = Stanza.iq(attrs, child, state)
+    assert {:error, [{_, resp}]} = Stanza.iq(attrs, child, state)
     resp = IO.chardata_to_string(resp)
 
     assert resp =~ ~s(<stream:error)
@@ -251,7 +251,7 @@ defmodule Egapp.XMPP.StanzaTest do
     attrs = %{"type" => "foo", "id" => state.id}
     child = {"ping", %{"xmlns" => Const.xmlns_ping()}, []}
 
-    assert {:error, resp} = Stanza.iq(attrs, child, state)
+    assert {:error, [{_, resp}]} = Stanza.iq(attrs, child, state)
     resp = IO.chardata_to_string(resp)
 
     assert resp =~ ~s(<stream:error)