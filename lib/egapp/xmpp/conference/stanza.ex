a4e80f25b7efc6954f52998ba3a364c932b971ce
Mahyar Abbaspour <mahyar.abaspour@gmail.com>
2020-03-20 14:39:48 +0330

    Add support for vcard to conference

--- lib/egapp/xmpp/conference/stanza.ex
+++ lib/egapp/xmpp/conference/stanza.ex
@@ -1,6 +1,9 @@
 defmodule Egapp.XMPP.Conference.Stanza do
+  require Egapp.Constants, as: Const
+
   import Egapp.XMPP.Stanza, only: [iq_template: 2, build_iq_attrs: 3]
 
+  alias Egapp.XMPP.Element
   alias Egapp.XMPP.Conference.Query
 
   def iq(%{"type" => "get"} = attrs, {"query", child_attrs, child_data}, state) do
@@ -17,6 +20,14 @@ defmodule Egapp.XMPP.Conference.Stanza do
       end)
 
     {:ok, resp}
+  end
+
+  def iq(%{"type" => "get"} = attrs, {"vCard", %{"xmlns" => Const.xmlns_vcard()}, _child_data}, state) do
+    resp =
+      iq_template(build_iq_attrs(attrs, :ok, state), Element.vcard())
+      |> :xmerl.export_simple_element(:xmerl_xml)
+
+    {:ok, [{state.to, resp}]}
   end
 
   def presence(_attrs, _data, state) do
